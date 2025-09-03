8bb5ca6524d4a34d9c775a3ceeadd5da8e538642
小川大輔 <ogawadaisuke@ogawadaisukenoMacBook-Air.local>
2025-09-03 13:51:22 +0900

    gem verとテストエラーの解決

--- spec/requests/delivery_locations_spec.rb
+++ spec/requests/delivery_locations_spec.rb
@@ -1,16 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe "DeliveryLocations", type: :request do
+  let(:customer) { Customer.create!(
+    customer_code: "TEST001", 
+    company_name: "Test Customer", 
+    postal_code: "123-4567", 
+    address: "Test Address",
+    invoice_delivery_method: :electronic
+  ) }
+  let(:delivery_location) { DeliveryLocation.create!(name: "Test Location", address: "Test Address", customer: customer) }
+
   describe "GET /index" do
     it "returns http success" do
-      get "/delivery_locations/index"
+      get "/delivery_locations"
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "GET /show" do
     it "returns http success" do
-      get "/delivery_locations/show"
+      get "/delivery_locations/#{delivery_location.id}"
       expect(response).to have_http_status(:success)
     end
   end
@@ -24,29 +33,29 @@ RSpec.describe "DeliveryLocations", type: :request do
 
   describe "GET /edit" do
     it "returns http success" do
-      get "/delivery_locations/edit"
+      get "/delivery_locations/#{delivery_location.id}/edit"
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET /create" do
-    it "returns http success" do
-      get "/delivery_locations/create"
-      expect(response).to have_http_status(:success)
+  describe "POST /create" do
+    it "creates a new delivery location" do
+      post "/delivery_locations", params: { delivery_location: { name: "New Location", address: "New Address", customer_id: customer.id } }
+      expect(response).to have_http_status(:redirect)
     end
   end
 
-  describe "GET /update" do
-    it "returns http success" do
-      get "/delivery_locations/update"
-      expect(response).to have_http_status(:success)
+  describe "PATCH /update" do
+    it "updates the delivery location" do
+      patch "/delivery_locations/#{delivery_location.id}", params: { delivery_location: { name: "Updated Location" } }
+      expect(response).to have_http_status(:redirect)
     end
   end
 
-  describe "GET /destroy" do
-    it "returns http success" do
-      get "/delivery_locations/destroy"
-      expect(response).to have_http_status(:success)
+  describe "DELETE /destroy" do
+    it "destroys the delivery location" do
+      delete "/delivery_locations/#{delivery_location.id}"
+      expect(response).to have_http_status(:redirect)
     end
   end
 end
--- spec/requests/product_categories_spec.rb
+++ spec/requests/product_categories_spec.rb
@@ -1,16 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe "ProductCategories", type: :request do
+  let(:product_category) { ProductCategory.create!(name: "Test Category", code: "TEST001") }
+
+  before do
+    # 認証をスキップ
+    allow_any_instance_of(ProductCategoriesController).to receive(:require_login).and_return(true)
+  end
+
   describe "GET /index" do
     it "returns http success" do
-      get "/product_categories/index"
+      get "/product_categories"
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "GET /show" do
     it "returns http success" do
-      get "/product_categories/show"
+      get "/product_categories/#{product_category.id}"
       expect(response).to have_http_status(:success)
     end
   end
@@ -24,29 +31,29 @@ RSpec.describe "ProductCategories", type: :request do
 
   describe "GET /edit" do
     it "returns http success" do
-      get "/product_categories/edit"
+      get "/product_categories/#{product_category.id}/edit"
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET /create" do
-    it "returns http success" do
-      get "/product_categories/create"
-      expect(response).to have_http_status(:success)
+  describe "POST /create" do
+    it "creates a new product category" do
+      post "/product_categories", params: { product_category: { name: "New Category", code: "NEW001" } }
+      expect(response).to have_http_status(:redirect)
     end
   end
 
-  describe "GET /update" do
-    it "returns http success" do
-      get "/product_categories/update"
-      expect(response).to have_http_status(:success)
+  describe "PATCH /update" do
+    it "updates the product category" do
+      patch "/product_categories/#{product_category.id}", params: { product_category: { name: "Updated Category" } }
+      expect(response).to have_http_status(:redirect)
     end
   end
 
-  describe "GET /destroy" do
-    it "returns http success" do
-      get "/product_categories/destroy"
-      expect(response).to have_http_status(:success)
+  describe "DELETE /destroy" do
+    it "destroys the product category" do
+      delete "/product_categories/#{product_category.id}"
+      expect(response).to have_http_status(:redirect)
     end
   end
 end
